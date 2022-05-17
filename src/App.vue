f943b0c7e088b3139e8e01241c25a9e35cdaeed6
hendrix <leandrohendrix@gmail.com>
2022-05-17 19:35:02 -0300

    Painel

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,15 @@
   <div class="corpo">
     <h1 class="centralizado">{{ title }}</h1>
     <h2 class="centralizado" v-text="subTitle"></h2>
-    <ul class="lista-fotos" v-for="photo in photos">
-      <li><img :src="photo.url" v-bind:alt="photo.alt"></li>
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="photo in photos">        
+        <div class="painel">
+          <h2 class="painel-titulo">{{photo.titulo}}</h2>
+          <div class="painel-corpo">
+            <img class="imagem-responsiva" :src="photo.url" v-bind:alt="photo.alt">
+          </div>
+        </div>
+      </li>
     </ul>
   </div>  
 </template>
@@ -41,7 +48,32 @@ export default {
      list-style: none;
    }
 
-   .lista-fotos{
-     display: inline-block;
-   }
+   .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .imagem-responsiva {
+    width: 100%;
+  }
+
+  .painel {
+    padding: 0 auto;
+    border: solid 2px grey;
+    display: inline-block;
+    margin: 5px;
+    box-shadow: 5px 5px 10px grey;
+    width: 200px;
+    height: 100%;
+    vertical-align: top;
+    text-align: center;
+  }
+
+  .painel .painel-titulo {
+    text-align: center;
+    border: solid 2px;
+    background: lightblue;
+    margin: 0 0 15px 0;
+    padding: 10px;
+    text-transform: uppercase;
+  }
 </style>
