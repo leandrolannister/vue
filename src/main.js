d8eb09888d718a1f7bf8ffb1b7e4699026bf80dd
hendrix <leandrohendrix@gmail.com>
2022-06-20 14:54:40 -0300

    Routes

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource';
 import VueRouter from 'vue-router';
+import { routes } from './routes.js';
 
 Vue.use(VueResource);
 Vue.use(VueRouter);
 
+const router =  new VueRouter({ routes });
+
 new Vue({
   el: '#app',
+  router:router,  
   render: h => h(App)
 })
