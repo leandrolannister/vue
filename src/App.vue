3d15ca4b6ee016d485a90ba66b4f2e15d0a73002
hendrix <leandrohendrix@gmail.com>
2022-06-22 09:17:27 -0300

    Transition

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,11 @@
         <router-link :to="route.path ? route.path : '/'">{{route.titulo}}</router-link>
       </li>
     </nav>
-    <router-view></router-view>
+
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
+    
   </div>  
 </template>
 
@@ -23,5 +27,13 @@
      font-family: Helvetica, sans-serif;
      width: 96%;
      margin: 0 auto;
+   }
+
+   .pagina-enter-active, .pagina-leave-active {
+     transition: opacity .3s
+   }
+   
+   .pagina-enter, .pagina-leave-active {
+     opacity: 0
    }   
 </style>
